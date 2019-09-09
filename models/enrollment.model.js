e44e680e7491a8a1f3e7c425592c56203d2d327d
Mateus <mateusholzschuh@hotmail.com>
2019-09-09 18:20:25 -0300

    codigo auto-incremento inscrição

--- models/enrollment.model.js
+++ models/enrollment.model.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let EnrollmentSchema = new Schema({
-    user : {
+    code: {
+        type: Number,
+    },
+
+    user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
@@ -16,6 +20,41 @@ let EnrollmentSchema = new Schema({
 
 }, {
     timestamps: true
+});
+
+EnrollmentSchema.pre('save', function (next) {
+    // quando a inscrição é criada
+    if (this.isNew) {
+        mongoose.model('Enrollment', EnrollmentSchema)
+            .find({ event: this.event })    // filtra pelo mesmo evento
+            .countDocuments()   // conta quantas tem
+            .then(res => {
+                // não tem inscrições ainda
+                if (res === 0) {
+                    this.code = 1; // recebe o primeiro codigo
+                    next();
+                }
+                else {
+                    mongoose.model('Enrollment', EnrollmentSchema)
+                        .find({ event: this.event })    // filtra pelo mesmo evento
+                        .select('code') // otimização
+                        .sort('-code')  // ordena pelo maior code
+                        .limit(1)   // pega o primeiro [maior]
+                        .exec()
+                        .then(max => {
+                            this.code = max[0].code + 1; // incrementa
+                            next();
+                        })
+                        .catch(err => { // caso de erro
+                            console.error(err);
+                            this.code = -1;
+                            next();
+                        })
+                }
+            });
+    } else {
+        next();
+    }
 });
 
 // Exporta o modelo
