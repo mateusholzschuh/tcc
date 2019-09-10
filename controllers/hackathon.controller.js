67570dc3edd18e7c1376e9f3bd7cdf257f9f24df
Mateus <mateusholzschuh@hotmail.com>
2019-09-10 02:26:24 -0300

    front - hackathon  init

--- controllers/hackathon.controller.js
+++ controllers/hackathon.controller.js
@@ -8,29 +8,26 @@ const moment = require('moment');
  */
 const index = async (req, res, next) => {
     let event = await Event.findOne({ _id: req.params.id })
-    .select('hackathon enrolleds')
-    .populate([{
-        path: 'hackathon',
-        populate: {
-            path: 'members',
-            select: 'name cpf'
-        }
-    },{
+    .select('enrolleds')
+    .populate({
         path: 'enrolleds',
-        select: 'user -_id',
         populate: {
-            path: 'user',
+            path:'user',
             select: 'name cpf'
         }
-    }])
+    })
     .exec().catch(err => {
         console.error(err)
         return;
     });
 
+    let hackathon = await Hackathon.findOne( {event: req.params.id }).populate({
+        path: 'teams.members',
+        select: 'name cpf'
+    }).exec()
 //     res.json(event)
 // return
-    let users = event.enrolleds
+    let users = event.enrolleds.map(e => e.user)
 
     //let event = await Event.findOne({ _id: req.params.id }).exec()
     // let event = await Event.find({}).exec().catch(err => console.error(err))
@@ -45,7 +42,7 @@ const index = async (req, res, next) => {
 
     res.render('events/event/hackathon/index', {
         title: 'Hackathon',
-        list: event.hackathon,
+        list: hackathon.teams,
         users: users,
         moment: moment, // biblioteca formatar data
         // menu: [
@@ -63,31 +60,21 @@ const index = async (req, res, next) => {
  */
 const store = async (req, res, next) => {
     //res.send("Olá mundo @store vindo do controller <strong>'Lecture'</strong>");
-    const { name, description, location, date, limit, confirmed, speakers } = req.body;
+    const { name, member1, member2, member3 } = req.body;
     const event = req.params.id;
 
-    let hackathon = {
+    let team = {
         name,
-        description,
-        location,
-        date: moment(date, 'DD/MM/YYYY - HH:mm'),
-        limit: Number(limit) || 1,
-        confirmed: confirmed == 'on' ? true : false,
-        speakers,
-        event
+        members: [member1, member2, member3]
     }
 
     // res.json({lecture: lecture})
 
-    Hackathon.create(hackathon).then(r => {
-        // res.json(r)
-        Event.findOneAndUpdate({ _id: event }, { '$push': { 'hackathon': r._id } }).then(ok => {
-
-            res.redirect('./hackathon');
-        })
-    }).catch(e => {
-        res.status(500).json(e);
-    });
+    Hackathon.findOneAndUpdate({ event: event }, { '$push' : { 'teams' : team } }).then(doc => {
+        res.redirect('./hackathon')
+    }).catch(err => {
+        res.redirect('./', 400)
+    })
 
 };
 
@@ -113,52 +100,6 @@ const edit = async (req, res, next) => {
             moment: moment, // biblioteca
         })
     });
-
-};
-
-/**
- * Função responsável por enviar a lista de inscritos e o form de inscrever
- */
-const enrolleds = async (req, res, next) => {
-    let hackathon= await Hackathon.findById(req.params.hackathon)
-        .populate({
-            path: 'enrolleds.user',
-            select: 'name cpf'
-        }).exec();
-
-    let users = await User.find({ _id : {'$nin' : hackathonenrolleds } }).select('name cpf').exec();
-
-    // res.json(hackathon
-// return
-    res.render('events/event/hackathon/enrolleds', {
-        title: hackathonname || 'Inscritos na oficina',
-        turl: '/events/'+req.params.id+'/hackathon',    
-        list: hackathonenrolleds,
-        users: users,
-        moment: moment, // biblioteca formatar data
-        // menu: [
-        //     {
-        //         title: 'Adicionar nova',
-        //         url: `/events/${req.params.id}/lectures/create`,
-        //         icon: 'add'
-        //     }
-        // ]
-    });
-};
-
-/**
- * Função responsável por inscrever cidadão na oficina 
- */
-const enroll = (req, res, next) => {
-    const { user } = req.body;
-    const hackathon= req.params.hackathon
-
-    Hackathon.findByIdAndUpdate(hackathon, { '$push' : { 'enrolleds' : { user: user } } }).then(doc => {
-        res.json(doc)
-    }).catch(err => {
-        res.json(err);
-        // next();
-    })
 
 };
 
@@ -193,12 +134,12 @@ const update = (req, res, next) => {
  * Função responsável por deletar o item do banco de dados
  */
 const destroy = (req, res, next) => {
-    Event.findByIdAndUpdate(req.params.id, { '$pull' : { 'lectures' : req.params.lecture} }).exec().then(doc => {
-        Lecture.findByIdAndDelete(req.params.lecture).then(doc => {
-            res.redirect(`/events/${req.params.id}/lectures`)
-        })
+    Hackathon.findOneAndUpdate({ event: req.params.id }, { '$pull' : { 'teams' : {'_id' : req.params.team} } }).then(doc => {
+        res.redirect(`/events/${req.params.id}/hackathon`)
+    }).catch(err => {
+        res.redirect('../../hackathon')
     })
 };
 
 // exporta as funções
-module.exports = { index, store, view, edit, update, destroy, enrolleds, enroll };
+module.exports = { index, store, view, edit, update, destroy };
--- routes/hackathon.routes.js
+++ routes/hackathon.routes.js
@@ -8,12 +8,10 @@ const controller = require('../controllers/hackathon.controller');
 // rotas do resource
 router.get('/',         controller.index);
 router.post('/',        controller.store);
-router.get('/:id',      controller.view);
-router.get('/:workshop/edit', controller.edit);
-router.post('/:workshop/edit/',controller.update);
-router.get('/:workshop/delete',  controller.destroy);
-router.get('/:workshop/enrolleds',  controller.enrolleds);
-router.post('/:workshop/enrolleds',  controller.enroll)
+// router.get('/:id',      controller.view);
+// router.get('/:workshop/edit', controller.edit);
+// router.post('/:workshop/edit/',controller.update);
+router.get('/:team/delete',  controller.destroy);
 
 // exporta o router
 module.exports = router;
