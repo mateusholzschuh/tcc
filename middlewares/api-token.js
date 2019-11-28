d1bde6b95fe5f036db954026c51a0f0ae308d120
Mateus <mateusholzschuh@hotmail.com>
2019-11-28 12:51:45 -0300

    add api auth (token)

--- middlewares/api-token.js
+++ middlewares/api-token.js
@@ -2,25 +2,18 @@ const Event = require('../services/event')
 
 module.exports = async (req, res, next) => {
 
-        let event = req.params.id
-    
-        try {
-            let config = await Event.getApiConfig(event)
-    
-            if (config.global && config[action]) {
-                return next()
-            }
+    let event = req.params.id
 
-            switch (action) {
-                case 'enroll':
-                    throw 'Inscrições encerradas'
-                    break
-                default:
-                    throw 'Rota não permitida'
-            }
-    
-        } catch (e) {
-            return res.status(400).json({ errors: [e] })
+    try {
+        let config = await Event.getApiConfig(event)
+
+        if (req.headers.authorization == 'Bearer '.concat(config.token)) {
+            return next()
         }
+        
+        throw 'Rota não permitida'
+
+    } catch (e) {
+        return res.status(400).json({ errors: [e] })
     }
 }
--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -3,25 +3,26 @@ const controller = require('../../../controllers/event.controller')
 
 const validators = {
     isOpen: require('../../../middlewares/api-is-open'),
+    hasToken: require('../../../middlewares/api-token'),
     enroll: require('../../../middlewares/validators/enrollments/api')
 }
 
 router.get('/', controller.api.getAll)
 
-router.get('/:id', validators.isOpen('global'), controller.api.getById)
+router.get('/:id', validators.hasToken, validators.isOpen('global'), controller.api.getById)
 
-router.get('/:id/full', validators.isOpen('global'), controller.api.getByIdFull)
+router.get('/:id/full', validators.hasToken, validators.isOpen('global'), controller.api.getByIdFull)
 
-router.get('/:id/lectures', validators.isOpen('getLectures'), controller.api.getLectures)
+router.get('/:id/lectures', validators.hasToken, validators.isOpen('getLectures'), controller.api.getLectures)
 
-router.get('/:id/workshops', validators.isOpen('getWorkshops'), controller.api.getWorkshops)
+router.get('/:id/workshops', validators.hasToken, validators.isOpen('getWorkshops'), controller.api.getWorkshops)
 
-router.get('/:id/workshops/:workshop', validators.isOpen('getWorkshops'), controller.api.getWorkshop)
+router.get('/:id/workshops/:workshop', validators.hasToken, validators.isOpen('getWorkshops'), controller.api.getWorkshop)
 
-router.get('/:id/enrolleds', validators.isOpen('getEnrolleds'), controller.api.getEnrolleds)
+router.get('/:id/enrolleds', validators.hasToken, validators.isOpen('getEnrolleds'), controller.api.getEnrolleds)
 
-router.post('/:id/enroll', validators.isOpen('enroll'), validators.enroll.onSave, controller.api.postEnroll)
+router.post('/:id/enroll', validators.hasToken, validators.isOpen('enroll'), validators.enroll.onSave, controller.api.postEnroll)
 
-router.post('/:id/check', validators.isOpen('check'), validators.enroll.onCheck, controller.api.postCheck)
+router.post('/:id/check', validators.hasToken, validators.isOpen('check'), validators.enroll.onCheck, controller.api.postCheck)
 
 module.exports = router
